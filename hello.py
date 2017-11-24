b15867a6f501d8fa59c6a399aba8104f154adbd0
韩伟 <hanwei@allinmd.cn>
2017-11-24 13:43:33 +0800

    add hello

---
+++ hello.py
@@ -0,0 +1,10 @@
+Git is a version control system.
+Git is free software.
+change
+change again
+Git has a mutable index called stage.
+Git tracks changes.
+test remote to github
+Creating a new branch is quick.
+Creating a new branch is quick AND simple.
+--no-ff
